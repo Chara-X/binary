8e8800da486c229108022f0e67e9da98034fbcc1
魏攀6092004007 <wei.pan2@zte.com.cn>
2024-11-07 18:18:50 +0800

    Up

---
+++ README.md
@@ -0,0 +1,5 @@
+# binary
+
+## Reference
+
+[Optimized bit-level Reader and Writer for Go](https://github.com/icza/bitio)
--- Reader.go
+++ Reader.go
@@ -6,4 +6,6 @@ type Reader struct {
 	io.Reader
 }
 
-func (r *Reader) ReadBits(n uint8) (u uint64, err error)
+func (r *Reader) ReadBits(n uint8) (u uint64, err error) {
+	panic("implement me")
+}
